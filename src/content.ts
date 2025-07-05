10a7c6869540303ef1f0c631270d029a6d6fc282
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-07-05 17:07:20 +0900

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -7,27 +7,30 @@ import ContentRuntime from "./runtime/content/content";
 import injectJs from "../dist/inject.js";
 import { randomString } from "./pkg/utils/utils";
 
-const internalMessage = new MessageInternal("content");
+const D392EC6BF3123287 = () => {
+  const internalMessage = new MessageInternal("content");
 
-const logger = new LoggerCore({
-  debug: process.env.NODE_ENV === "development",
-  writer: new MessageWriter(internalMessage),
-  labels: { env: "content", href: window.location.href },
-});
+  const logger = new LoggerCore({
+    debug: process.env.NODE_ENV === "development",
+    writer: new MessageWriter(internalMessage),
+    labels: { env: "content", href: window.location.href },
+  });
 
-const scriptFlag = randomString(8);
+  const scriptFlag = randomString(8);
 
-// 注入运行框架
-const temp = document.createElementNS("http://www.w3.org/1999/xhtml", "script");
-temp.setAttribute("type", "text/javascript");
-temp.innerHTML = `(function (ScriptFlag) {\n${injectJs}\n})('${scriptFlag}')`;
-temp.className = "injected-js";
-document.documentElement.appendChild(temp);
-temp.remove();
+  // 注入运行框架
+  const temp = document.createElementNS("http://www.w3.org/1999/xhtml", "script");
+  temp.setAttribute("type", "text/javascript");
+  temp.textContent = `(function (ScriptFlag) {\n${injectJs}\n})('${scriptFlag}')`;
+  temp.className = "injected-js";
+  document.documentElement.appendChild(temp);
+  temp.remove();
 
-internalMessage.syncSend("pageLoad", null).then((resp) => {
-  logger.logger().debug("content start");
-  // 通过flag与inject建立通讯
-  const contentMessage = new MessageContent(scriptFlag, true);
-  new ContentRuntime(contentMessage, internalMessage).start(resp);
-});
+  internalMessage.syncSend("pageLoad", null).then((resp) => {
+    logger.logger().debug("content start");
+    // 通过flag与inject建立通讯
+    const contentMessage = new MessageContent(scriptFlag, true);
+    new ContentRuntime(contentMessage, internalMessage).start(resp);
+  });
+};
+D392EC6BF3123287();
