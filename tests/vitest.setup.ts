19cb2f507f4c393b784197c912c8d70677dd53f2
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-07-12 16:15:18 +0900

    .

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,3 +1,18 @@
 import chromeMock from "@Packages/chrome-extension-mock";
 
 chromeMock.init();
+
+if (!("onanimationstart" in global)) {
+    // Define or mock the global handler
+    let val: any = null;
+    Object.defineProperty(global, "onanimationstart", {
+        configurable: true,
+        writable: true,
+        set(newVal) {
+            val = newVal;
+        },
+        get() {
+            return val;
+        }
+    });
+}
