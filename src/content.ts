85606630dec1a213957e848eb92d85dab8509c0a
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-12-06 15:23:08 +0900

    1

--- src/content.ts
+++ src/content.ts
@@ -1,51 +1,212 @@
-import LoggerCore from "./app/logger/core";
+import LoggerCore from "@App/app/logger/core";
+import type { Logger } from "@App/app/repo/logger";
 import MessageWriter from "./app/logger/message_writer";
 import { ExtensionMessage } from "@Packages/message/extension_message";
 import { CustomEventMessage } from "@Packages/message/custom_event_message";
 import { Server } from "@Packages/message/server";
-import ContentRuntime from "./app/service/content/content";
 import { ScriptExecutor } from "./app/service/content/script_executor";
-import { randomMessageFlag, getUspMessageFlag } from "./pkg/utils/utils";
+import { randomMessageFlag } from "./pkg/utils/utils";
 import type { Message } from "@Packages/message/types";
+import { forwardMessage } from "@Packages/message/server";
+import { makeBlobURL } from "@App/pkg/utils/utils";
+import { RuntimeClient } from "@App/app/service/service_worker/client";
+import type { TScriptInfo } from "@App/app/repo/scripts";
+import type { GMInfoEnv } from "@App/app/service/content/types";
 
-// @ts-ignore
-const MessageFlag: string | null = (typeof arguments === "object" && arguments?.[0]) || getUspMessageFlag();
+// 建立与service_worker页面的连接
+// 发送给扩展service_worker的通信接口
+const extMsgComm: Message = new ExtensionMessage(false);
+// 初始化日志组件
+const loggerCore = new LoggerCore({
+  writer: new MessageWriter(extMsgComm, "serviceWorker/logger"),
+  labels: { env: "content" },
+});
 
-if (!MessageFlag) {
-  console.error("MessageFlag is unavailable.");
-} else if (typeof chrome?.runtime?.onMessage?.addListener !== "function") {
+loggerCore.logger().debug("content start");
+
+// 处理scriptExecutor
+const scriptExecutorFlag = randomMessageFlag();
+// 脚本执行器消息接口
+const scriptExecutorMsg = new CustomEventMessage(scriptExecutorFlag, true);
+const scriptExecutor = new ScriptExecutor(new CustomEventMessage(scriptExecutorFlag, false));
+
+const listenContentMessage = (key: string, runtimeMessageFlag: string, callback: (data: any) => any) => {
+  performance.addEventListener(`scriptcat-content-${key}`, (ev) => {
+    if (ev instanceof CustomEvent) {
+      const detail = ev.detail;
+      if (detail && typeof detail === "object") {
+        const { messageFlag, messageData } = detail;
+        if (messageFlag !== runtimeMessageFlag) return;
+        callback(messageData);
+      }
+    }
+  });
+};
+
+const forwardGMApi = (data: { api: string; params: any; uuid: string }) => {
+  // 拦截关注的api
+  switch (data.api) {
+    case "CAT_createBlobUrl": {
+      const file = data.params[0] as File;
+      const url = makeBlobURL({ blob: file, persistence: false }) as string;
+      return url;
+    }
+    case "CAT_fetchBlob": {
+      return fetch(data.params[0]).then((res) => res.blob());
+    }
+    case "CAT_fetchDocument": {
+      return new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "document";
+        xhr.open("GET", data.params[0]);
+        xhr.onload = () => {
+          const nodeId = (this.senderToInject as CustomEventMessage).sendRelatedTarget(xhr.response);
+          resolve(nodeId);
+        };
+        xhr.send();
+      });
+    }
+    case "GM_addElement": {
+      const [parentNodeId, tagName, tmpAttr] = data.params;
+      let attr = { ...tmpAttr };
+      let parentNode: EventTarget | undefined;
+      // 判断是不是content脚本发过来的
+      let msg: CustomEventMessage;
+      if (contentScriptSet.has(data.uuid) || scriptExecutor.execMap.has(data.uuid)) {
+        msg = scriptExecutorMsg;
+      } else {
+        msg = this.senderToInject;
+      }
+      if (parentNodeId) {
+        parentNode = msg.getAndDelRelatedTarget(parentNodeId);
+      }
+      const el = <Element>document.createElement(tagName);
+
+      let textContent = "";
+      if (attr) {
+        if (attr.textContent) {
+          textContent = attr.textContent;
+          delete attr.textContent;
+        }
+      } else {
+        attr = {};
+      }
+      for (const key of Object.keys(attr)) {
+        el.setAttribute(key, attr[key]);
+      }
+      if (textContent) {
+        el.textContent = textContent;
+      }
+      (<Element>parentNode || document.head || document.body || document.querySelector("*")).appendChild(el);
+      const nodeId = msg.sendRelatedTarget(el);
+      return nodeId;
+    }
+    case "GM_log":
+      // 拦截GM_log，打印到控制台
+      // 由于某些页面会处理掉console.log，所以丢到这里来打印
+      switch (data.params.length) {
+        case 1:
+          console.log(data.params[0]);
+          break;
+        case 2:
+          console.log("[" + data.params[1] + "]", data.params[0]);
+          break;
+        case 3:
+          console.log("[" + data.params[1] + "]", data.params[0], data.params[2]);
+          break;
+      }
+      break;
+  }
+  return false;
+};
+
+// 运行在content页面的脚本
+const contentScriptSet = new Set<string>();
+
+listenContentMessage("pageLoad", MessageFlag, (data: { contentScriptList: TScriptInfo[]; envInfo: GMInfoEnv }) => {
+  const { contentScriptList, envInfo } = data;
+  // 处理注入到content环境的脚本
+  for (const script of contentScriptList) {
+    contentScriptSet.add(script.uuid);
+  }
+  // 监听事件
+  scriptExecutor.setEnvInfo(envInfo);
+  // 启动脚本
+  scriptExecutor.startScripts(contentScriptList);
+});
+
+const contentRuntimePageLoad = (messageFlag: string, submitPageLoad: (data: any) => any) => {
+  scriptExecutor.checkEarlyStartScript("content", messageFlag);
+  const client = new RuntimeClient(extMsgComm);
+  // 向service_worker请求脚本列表及环境信息
+  client.pageLoad().then((o) => {
+    if (!o.ok) return;
+    const { injectScriptList, contentScriptList, envInfo } = o;
+    submitPageLoad({ injectScriptList, contentScriptList, envInfo });
+  });
+};
+
+if (typeof chrome?.runtime?.onMessage?.addListener !== "function") {
   // Firefox userScripts.RegisteredUserScript does not provide chrome.runtime.onMessage.addListener
   // Firefox scripting.RegisteredContentScript does provide chrome.runtime.onMessage.addListener
   // Firefox 的 userScripts.RegisteredUserScript 不提供 chrome.runtime.onMessage.addListener
   // Firefox 的 scripting.RegisteredContentScript 提供 chrome.runtime.onMessage.addListener
   console.error("chrome.runtime.onMessage.addListener is not a function");
 } else {
-  // 建立与service_worker页面的连接
-  const extMsgComm: Message = new ExtensionMessage(false);
-  // 初始化日志组件
-  const loggerCore = new LoggerCore({
-    writer: new MessageWriter(extMsgComm, "serviceWorker/logger"),
-    labels: { env: "content" },
+  let MessageFlag = "";
+
+  chrome.storage.local.get(["storedMessageFlag"]).then((result) => {
+    MessageFlag = result?.storedMessageFlag || "";
   });
 
-  loggerCore.logger().debug("content start");
+  chrome.storage.session.onChanged.addListener((changes: Record<string, chrome.storage.StorageChange>) => {
+    const keys = Object.keys(changes);
+    for (const key of keys) {
+      const messagePayload = changes[key]?.newValue;
+      if (!messagePayload) continue;
+      performance.dispatchEvent(
+        new CustomEvent(`scriptcat-content-${key}`, {
+          detail: {
+            messageFlag: MessageFlag,
+            messageData: messagePayload.data,
+          },
+        })
+      );
+    }
+  });
 
-  const msgInject = new CustomEventMessage(MessageFlag, true);
+  if (!MessageFlag) {
+    console.error("MessageFlag is unavailable.");
+  } else {
+    // 发送给inject的消息接口
+    const msgInject = new CustomEventMessage(MessageFlag, true);
 
-  // 处理scriptExecutor
-  const scriptExecutorFlag = randomMessageFlag();
-  const scriptExecutorMsg = new CustomEventMessage(scriptExecutorFlag, true);
-  const scriptExecutor = new ScriptExecutor(new CustomEventMessage(scriptExecutorFlag, false));
+    // 监听来自inject的消息
+    const server = new Server("content", [msgInject, scriptExecutorMsg]);
 
-  const server = new Server("content", [msgInject, scriptExecutorMsg]);
+    listenContentMessage("emitEvent", MessageFlag, (data) => {
+      scriptExecutor.emitEvent(data);
+    });
+    listenContentMessage("valueUpdate", MessageFlag, (data) => {
+      scriptExecutor.valueUpdate(data);
+    });
+    server.on("logger", (data: Logger) => {
+      LoggerCore.logger().log(data.level, data.message, data.label);
+    });
+    forwardMessage("serviceWorker", "script/isInstalled", server, msgInject);
+    forwardMessage("serviceWorker", "runtime/gmApi", server, msgInject, forwardGMApi);
 
-  // Opera中没有chrome.runtime.onConnect，并且content也不需要chrome.runtime.onConnect
-  // 所以不需要处理连接，设置为false
-  const extServer = new Server("content", extMsgComm, false);
-  // scriptExecutor的消息接口
-  // 初始化运行环境
-  const runtime = new ContentRuntime(extServer, server, extMsgComm, msgInject, scriptExecutorMsg, scriptExecutor);
-  runtime.init();
-  // 页面加载，注入脚本
-  runtime.pageLoad(MessageFlag);
+    // 页面加载，注入脚本
+    contentRuntimePageLoad(MessageFlag, (data: any) => {
+      const key = "pageLoad";
+      performance.dispatchEvent(
+        new CustomEvent(`scriptcat-content-${key}`, {
+          detail: {
+            messageFlag: MessageFlag,
+            messageData: data,
+          },
+        })
+      );
+    });
+  }
 }
--- src/inject.ts
+++ src/inject.ts
@@ -1,16 +1,87 @@
-import LoggerCore from "./app/logger/core";
-import MessageWriter from "./app/logger/message_writer";
+import LoggerCore from "@App/app/logger/core";
+import MessageWriter from "@App/app/logger/message_writer";
 import { CustomEventMessage } from "@Packages/message/custom_event_message";
-import { Server } from "@Packages/message/server";
-import type { TScriptInfo } from "./app/repo/scripts";
-import type { GMInfoEnv } from "./app/service/content/types";
-import { InjectRuntime } from "./app/service/content/inject";
+import type { TScriptInfo } from "@App/app/repo/scripts";
+import type { GMInfoEnv, ValueUpdateDataEncoded } from "./app/service/content/types";
 import { ScriptExecutor } from "./app/service/content/script_executor";
 import type { Message } from "@Packages/message/types";
+import { ExternalWhitelist } from "@App/app/const";
+import { sendMessage } from "@Packages/message/client";
+import type { EmitEventRequest } from "@App/app/service/service_worker/types";
 
 /* global MessageFlag  */
 
 const msg: Message = new CustomEventMessage(MessageFlag, false);
+const scriptExecutor = new ScriptExecutor(msg);
+
+const listenContentMessage = (key: string, runtimeMessageFlag: string, callback: (data: any) => any) => {
+  performance.addEventListener(`scriptcat-content-${key}`, (ev) => {
+    if (ev instanceof CustomEvent) {
+      const detail = ev.detail;
+      if (detail && typeof detail === "object") {
+        const { messageFlag, messageData } = detail;
+        if (messageFlag !== runtimeMessageFlag) return;
+        callback(messageData);
+      }
+    }
+  });
+};
+
+const setupExternalMessage = () => {
+  // 对外接口白名单
+  const hostname = window.location.hostname;
+  if (
+    ExternalWhitelist.some(
+      // 如果当前页面的 hostname 是白名单的网域或其子网域
+      (t) => hostname.endsWith(t) && (hostname.length === t.length || hostname.endsWith(`.${t}`))
+    )
+  ) {
+    // 注入
+    const external: External = window.external || (window.external = {} as External);
+    const scriptExpose: App.ExternalScriptCat = {
+      isInstalled(name: string, namespace: string, callback: (res: App.IsInstalledResponse | undefined) => unknown) {
+        sendMessage<App.IsInstalledResponse>(msg, "content/script/isInstalled", {
+          name,
+          namespace,
+        }).then(callback);
+      },
+    };
+    try {
+      external.Scriptcat = scriptExpose;
+    } catch {
+      // 无法注入到 external，忽略
+    }
+    const exposedTM = external.Tampermonkey;
+    const isInstalledTM = exposedTM?.isInstalled;
+    const isInstalledSC = scriptExpose.isInstalled;
+    if (isInstalledTM && exposedTM?.getVersion && exposedTM.openOptions) {
+      // 当TM和SC同时启动的特殊处理：如TM没有安装，则查SC的安装状态
+      try {
+        exposedTM.isInstalled = (
+          name: string,
+          namespace: string,
+          callback: (res: App.IsInstalledResponse | undefined) => unknown
+        ) => {
+          isInstalledTM(name, namespace, (res) => {
+            if (res?.installed) callback(res);
+            else
+              isInstalledSC(name, namespace, (res) => {
+                callback(res);
+              });
+          });
+        };
+      } catch {
+        // 忽略错误
+      }
+    } else {
+      try {
+        external.Tampermonkey = scriptExpose;
+      } catch {
+        // 无法注入到 external，忽略
+      }
+    }
+  }
+};
 
 // 加载logger组件
 const logger = new LoggerCore({
@@ -19,17 +90,22 @@ const logger = new LoggerCore({
   labels: { env: "inject", href: window.location.href },
 });
 
-const server = new Server("inject", msg);
-const scriptExecutor = new ScriptExecutor(msg);
-const runtime = new InjectRuntime(server, msg, scriptExecutor);
-runtime.init();
-// 检查early-start的脚本
-scriptExecutor.checkEarlyStartScript("inject", MessageFlag);
+listenContentMessage("emitEvent", MessageFlag, (data: EmitEventRequest) => {
+  // 转发给脚本
+  scriptExecutor.emitEvent(data);
+});
+listenContentMessage("valueUpdate", MessageFlag, (data: ValueUpdateDataEncoded) => {
+  // 转发给脚本
+  scriptExecutor.valueUpdate(data);
+});
 
-server.on("pageLoad", (data: { injectScriptList: TScriptInfo[]; envInfo: GMInfoEnv }) => {
+listenContentMessage("pageLoad", MessageFlag, (data: { injectScriptList: TScriptInfo[]; envInfo: GMInfoEnv }) => {
   logger.logger().debug("inject start");
   // 监听事件
-  runtime.setEnvInfo(data.envInfo);
-  runtime.startScripts(data.injectScriptList);
-  runtime.onInjectPageLoaded();
+  scriptExecutor.setEnvInfo(data.envInfo);
+  scriptExecutor.startScripts(data.injectScriptList);
+  setupExternalMessage();
 });
+
+// 检查early-start的脚本
+scriptExecutor.checkEarlyStartScript("inject", MessageFlag);
