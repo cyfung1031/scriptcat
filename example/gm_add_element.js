ad4d72378c7ae9d373a0d764086ff1f3ab8cc066
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-02-18 08:03:23 +0900

    测试改一下

--- example/gm_add_element.js
+++ example/gm_add_element.js
@@ -22,9 +22,89 @@
  * 2. 元素标签名
  * 3. 属性对象
  */
+
+// ------------- 基础用法（TM） B1 ----------------
+
 const el = GM_addElement(document.querySelector('.BorderGrid-cell'), "img", {
     src: "https://bbs.tampermonkey.net.cn/uc_server/avatar.php?uid=4&size=small&ts=1"
 });
 
 // 打印创建出来的 DOM 元素
 console.log(el);
+
+// ------------- 基础用法（TM） B2 - textContent ----------------
+
+
+const span3 = GM_addElement('span', {
+    textContent: 'Hello',
+});
+
+console.log(`span text: ${span3.textContent}`);
+
+// ------------- 基础用法（TM） B3 - onload & onerror ----------------
+
+
+new Promise((resolve, reject) => {
+    img = GM_addElement(document.body, 'img', {
+        src: 'https://www.tampermonkey.net/favicon.ico',
+        onload: resolve,
+        onerror: reject
+    });
+}).then(() => {
+    console.log("img insert ok");
+}).catch(() => {
+    console.log("img insert failed")
+});
+
+
+if (GM?.info.scriptHandler === "ScriptCat") {
+
+    // ------------- 額外用法（SC） E1 - value ----------------
+
+
+    const textarea = GM_addElement('textarea', {
+        value: "myText",
+    });
+
+    console.log(`Textarea Value: ${textarea.value}`);
+
+    // ------------- 額外用法（SC） E2 - innerHTML ----------------
+
+    const div3 = GM_addElement('div', {
+        innerHTML: '<div id="test777">World</div>',
+    });
+
+    console.log(`div text: ${div3.textContent}`);
+
+
+    // ------------- 額外用法（SC） E3 - className ----------------
+
+
+    const span4 = GM_addElement(document.getElementById("test777"), 'span', {
+        className: "test777-span",
+        textContent: 'Hello World!',
+    });
+
+    console.log(`span class: ${span4.classList.contains("test777-span")}`)
+
+
+
+    // ------------- 額外用法（SC） E4 - native ----------------
+
+    // 在目前环境生成元素
+
+    const elementA = GM_addElement('div', {
+        native: true,
+        textContent: "DEF",
+    });
+
+
+    // ------------- 額外用法（SC） E5 - insertBefore ----------------
+
+    // 插入在某元素前面 = parentNdoe.insertBefore(node, referenceNode)
+
+    const elementB = GM_addElement('textarea', {
+        value: "ABC",
+    }, elementA);
+
+}
